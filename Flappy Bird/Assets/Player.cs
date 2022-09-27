a94220dda2fcecc8523e6d85f3a7022cb5ed0203
Jahangir Alam Rocky <x00jahangir@gmail.com>
2022-09-28 00:57:11 +0600

    Added bird jump mechanics - Jump mechanics with rigidbody velocity

---
+++ Flappy Bird/Assets/Player.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    [SerializeField] private float jumpForce = 1f;
+    private Rigidbody2D rb;
+
+    private void Start() {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Jump() {
+        rb.velocity = Vector2.up * jumpForce;
+    }
+
+    private void Update() {
+        if (Input.GetMouseButtonDown(0)) {
+            Jump();
+        }
+    }
+}
