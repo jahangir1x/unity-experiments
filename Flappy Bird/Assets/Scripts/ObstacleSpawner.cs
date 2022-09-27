3841c41e9a05555eb1c1e358c284de3fe9a42ed5
Jahangir Alam Rocky <x00jahangir@gmail.com>
2022-09-28 01:31:13 +0600

    Added random obstacles spawning systme - moving obstacles added - Refactored codebase - Organized project structure

---
+++ Flappy Bird/Assets/Scripts/ObstacleSpawner.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class ObstacleSpawner : MonoBehaviour {
+    [SerializeField] private float obstacleDestroyTime = 10f;
+    [SerializeField] private GameObject obstaclePrefab;
+    [SerializeField] private float spawnPositionX = 1f;
+    [SerializeField] private float spawnPositionRangeY = 1f;
+
+    private float spawnRatePerSecond = 0.5f;
+    private float elapsedTimeSinceLastSpawn = 0f;
+
+    private void Start() {
+        GameObject newObstacle = Instantiate(obstaclePrefab,
+                                                transform.position,
+                                                Quaternion.identity);
+        newObstacle.transform.position = transform.position +
+                                            new Vector3(spawnPositionX,
+                                                Random.Range(-spawnPositionRangeY, spawnPositionRangeY),
+                                                0);   // spawn obstacle at random position.
+    }
+
+    private void Update() {
+        if (elapsedTimeSinceLastSpawn > 1f / spawnRatePerSecond) {  // if enough time has passed since last spawn, then spawn new obstacle.
+            GameObject newObstacle = Instantiate(obstaclePrefab,
+                                        transform.position,
+                                        Quaternion.identity);
+            newObstacle.transform.position = transform.position +
+                                                new Vector3(spawnPositionX,
+                                                    Random.Range(-spawnPositionRangeY, spawnPositionRangeY),
+                                                    0);   // spawn obstacle at random position.
+            elapsedTimeSinceLastSpawn = 0f; // set elapsed time to 0.
+            spawnRatePerSecond = Random.Range(0.4f, 0.6f); // set spawn rate to random value.
+            Destroy(newObstacle, obstacleDestroyTime);  // destroy obstacle after 10 seconds.
+        }
+
+        elapsedTimeSinceLastSpawn += Time.deltaTime;    // increment elapsed time.
+    }
+}
--- Flappy Bird/Assets/Scripts/Player.cs
+++ Flappy Bird/Assets/Scripts/Player.cs
@@ -15,8 +15,8 @@ public class Player : MonoBehaviour {
     }
 
     private void Update() {
-        if (Input.GetMouseButtonDown(0)) {
-            Jump();
+        if (Input.GetMouseButtonDown(0)) {  // if left mouse is pressed
+            Jump();                         //    then jump.
         }
     }
 }
