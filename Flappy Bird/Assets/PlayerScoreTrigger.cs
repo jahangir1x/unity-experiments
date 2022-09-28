c2156f6cfd97fc08d1543902e813ea73af5131ae
Jahangir Alam Rocky <x00jahangir@gmail.com>
2022-09-28 21:53:49 +0600

    Scoring system added

---
+++ Flappy Bird/Assets/PlayerScoreTrigger.cs
@@ -0,0 +1,15 @@
+﻿
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlayerScoreTrigger : MonoBehaviour {
+    private void OnTriggerEnter2D(Collider2D other) {
+        IncrementScore();
+    }
+
+    private void IncrementScore() {
+        var scoreUIText = GameManager.ScoreTextMesh;
+        scoreUIText.text = (int.Parse(scoreUIText.text) + 1).ToString();
+    }
+}
--- Flappy Bird/Assets/Scripts/GameManager.cs
+++ Flappy Bird/Assets/Scripts/GameManager.cs
@@ -1,10 +1,19 @@
 ﻿using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour {
+    [SerializeField] private TMPro.TextMeshProUGUI _scoreTextMesh;         // store score UI text mesh reference
+
+    public static TMPro.TextMeshProUGUI ScoreTextMesh;   // score UI text mesh reference as static field
+
+    // Layers
+    public static int PlayerLayer;      // store player layer id
     public static int ObstacleLayer;    // store obstacle layer id
 
     private void Start() {
+        PlayerLayer = LayerMask.NameToLayer("Player");
         ObstacleLayer = LayerMask.NameToLayer("Obstacle");
+        ScoreTextMesh = _scoreTextMesh;
     }
 
     // Game over functionality
