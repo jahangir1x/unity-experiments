eff536f6dc5272dc329b66da264be99bbc89a128
Jahangir Alam Rocky <x00jahangir@gmail.com>
2022-10-03 18:17:15 +0600

    Added item collection quest

---
+++ Flappy Bird/Assets/Scripts/ChestActivator.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PixelCrushers;
+
+// Implement IMessageHandler to receive message from quest machine message system
+public class ChestActivator : MonoBehaviour, IMessageHandler {
+
+    private void Start() {
+        MessageSystem.AddListener(this, "Spawn", "chests");     // Register listener
+    }
+
+    public void OnMessage(MessageArgs messageArgs) {        // Do things when proper messages is passed
+        foreach (Transform child in transform) {        // activate children
+            child.gameObject.SetActive(true);
+        }
+    }
+
+    private void OnDestroy() {
+        MessageSystem.RemoveListener(this, "Spawn", "chests");  // Remove listener
+    }
+
+
+}
---
+++ Flappy Bird/Assets/Scripts/ChestActivatorCheck.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PixelCrushers.QuestMachine;
+using PixelCrushers;
+
+public class ChestActivatorCheck : MonoBehaviour {
+    public GameObject NPC;
+    public Quest npcQuest;
+
+    private void Start() {
+        QuestGiver questGiver = NPC.GetComponent<QuestGiver>();
+        npcQuest = questGiver.questList[0];
+        Debug.Log(npcQuest.nodeList[1].internalName);
+    }
+
+    private void Update() {
+        if (npcQuest.GetNode("acquireChests").GetState() == QuestNodeState.Active) {
+            Debug.Log("acccccc");
+        }
+    }
+}
